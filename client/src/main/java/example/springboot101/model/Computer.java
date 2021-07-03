74270d69085139c30d67bacca444dfad781e5085
James Tan <largemtan@gmail.com>
2021-07-03 02:35:54 -0400

    add H2 settings, still have problem on model swagger

--- client/src/main/java/example/springboot101/model/Computer.java
+++ client/src/main/java/example/springboot101/model/Computer.java
@@ -1,14 +1,26 @@
 package example.springboot101.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Computer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ApiModelProperty(accessMode = ApiModelProperty.AccessMode.READ_ONLY, dataType = "java.lang.Long")
     private long id;
     private String name;
+
+    /**
+     * need a default constructor, it seems query is using it to populate the result.
+     */
+    public Computer() {
+    }
 
     public Computer(String name) {
         this.name = name;
@@ -16,6 +28,10 @@ public class Computer {
 
     public long getId() {
         return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
